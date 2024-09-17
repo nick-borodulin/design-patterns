295048d4d3ca05831d04283634763e0d8f544deb
nick-borodulin <nikitaborodulin@gmail.com>
2024-09-17 16:54:32 -0700

    Command pattern added.

---
+++ design_patterns/behavioral/command.py
@@ -0,0 +1,52 @@
+"""
+This example combines Command, a behavioral pattern and Composite, a structural pattern.
+"""
+from typing import Any, Self
+from abc import abstractmethod, ABC
+
+
+class Command(ABC):
+    """Command pattern"""
+
+    @abstractmethod
+    def execute(self) -> Any:
+        ...
+    
+class Component:
+    """Composite pattern"""
+
+    def add_child(self, child: Self) -> None:
+        pass
+    
+    def remove_child(self, child_index: int) -> None:
+        pass
+
+
+class Receiver:
+    """Command receiver"""
+
+    def action(self) -> str:
+        return "receiver_action"
+
+
+class SimpleCommand(Command, Component):
+
+    def __init__(self, receiver: Receiver) -> None:
+        self._receiver = receiver
+
+    def execute(self) -> str:
+        return self._receiver.action()
+    
+
+class CompositeCommand(Command, Component):
+    def __init__(self) -> None:
+        self._children: list[Any] = []
+
+    def add_child(self, child: Component) -> None:
+        self._children.append(child)
+
+    def remove_child(self, child_index: int) -> None:
+        self._children.pop(child_index)
+
+    def execute(self) -> Any:
+        return "".join([child.execute() for child in self._children])
---
+++ tests/behavioral/command_test.py
@@ -0,0 +1,33 @@
+from contextlib import AbstractContextManager, nullcontext
+import pytest
+from design_patterns.behavioral.command import CompositeCommand, SimpleCommand, Receiver
+
+def test_simple_command() -> None:
+    command = SimpleCommand(Receiver())
+    assert command.execute() == "receiver_action"
+
+@pytest.mark.parametrize("add_children,remove_children,exception_during_removal",
+                         [(1, 1, nullcontext()),
+                          (10, 10, nullcontext()),
+                          (1, 2, pytest.raises(IndexError))
+                          ])
+def test_composite_command(add_children: int,
+                           remove_children: int,
+                           exception_during_removal: AbstractContextManager) -> None:
+    command = CompositeCommand()
+    children = []
+    for _ in range(add_children):
+        new_child = SimpleCommand(Receiver())
+        children.append(new_child)
+        command.add_child(new_child)
+    assert command.execute() == "receiver_action" * add_children
+
+    assert len(children) == add_children
+
+    with exception_during_removal:
+        for _ in range(remove_children):
+            command.remove_child(0)
+            
+    
+    
+        
