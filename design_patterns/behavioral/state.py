bee56f8a51df05165305aa770e0daa4e65f19e41
nick-borodulin <nikitaborodulin@gmail.com>
2024-09-24 19:28:03 -0700

    Added "state" behavioral pattern.

---
+++ design_patterns/behavioral/state.py
@@ -0,0 +1,70 @@
+"""
+The idea is to implement state machine using a set of classes, each
+of which represents one state. Each class is a singleton. A "state" interface
+defines a method that passes control to the next state.
+"""
+from typing import Self, override
+from design_patterns.creation.singleton import SingletonMetaClass
+
+class IncorrectStateError(ValueError):
+    pass
+
+class State(metaclass=SingletonMetaClass):
+    def __init__(self, context: "Context") -> None:
+        self._context = context
+
+    def initialize(self) -> None:
+        raise IncorrectStateError
+
+    def open(self) -> None:
+        raise IncorrectStateError
+
+    def close(self) -> None:
+        raise IncorrectStateError
+
+    def change_state(self, new_state: Self) -> None:
+        self._context.state = new_state
+        
+
+class Init(State):
+    @override
+    def initialize(self) -> None:
+        # Do work
+        self.change_state(Open())
+
+
+class Open(State):
+    @override
+    def open(self) -> None:
+        # Do work
+        self.change_state(Close())
+
+
+class Close(State):
+    @override
+    def close(self) -> None:
+        # Do work
+        # Not changing the state
+        pass
+
+
+class Context:
+    def __init__(self) -> None:
+        self._state = Init(self)
+
+    @property
+    def state(self) -> State:
+        return self._state
+    
+    @state.setter
+    def state(self, new_state: State) -> None:
+        self._state = new_state
+
+    def initialize(self) -> None:
+        self.state.initialize()
+
+    def open(self) -> None:
+        self.state.open()
+
+    def close(self) -> None:
+        self.state.close()
---
+++ tests/behavioral/state.py
@@ -0,0 +1,45 @@
+from design_patterns.behavioral.state import Context, Close, IncorrectStateError, Init, Open
+import pytest
+
+
+def test_correct_state_transitions() -> None:
+    context = Context()
+    assert type(context.state) is Init
+    context.initialize()
+    assert type(context.state) is Open
+    context.open()
+    assert type(context.state) is Close
+    context.close()
+    assert type(context.state) is Close # Close doesn't change into anything else.
+
+
+def test_incorrect_state_transitions_in_init_state() -> None:
+    context = Context()
+    assert type(context.state) is Init
+    with pytest.raises(IncorrectStateError):
+        context.open()
+    with pytest.raises(IncorrectStateError):
+        context.close()
+    
+def test_incorrect_state_transitions_in_open_state() -> None:
+    context = Context()
+    assert type(context.state) is Init
+    context.initialize()
+    with pytest.raises(IncorrectStateError):
+        context.initialize()
+    with pytest.raises(IncorrectStateError):
+        context.close()
+
+def test_incorrect_state_transitions_in_closed_state() -> None:
+    context = Context()
+    assert type(context.state) is Init
+    context.initialize()
+    assert type(context.state) is Open
+    context.open()
+    assert type(context.state) is Close
+    context.close()
+    assert type(context.state) is Close
+    with pytest.raises(IncorrectStateError):
+        context.initialize()
+    with pytest.raises(IncorrectStateError):
+        context.open()
